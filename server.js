2d6b381b08d56722202e129d45d0182182b16aa5
Sai Ruthwik <sairuthwik2002@gmail.com>
2026-06-12 19:09:37 +0530

    beatzen

--- server.js
+++ server.js
@@ -246,11 +246,7 @@ app.get('/start', rateLimit, async (req, res) => {
   });
 });
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, '0.0.0.0', () => {
-//   console.log(`Server running on ${PORT}`);
-// });
-
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
